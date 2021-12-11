6efa2b343282848348aebef259facb4fb0ec71a7
David Grammatico <david.lugdunum@gmail.com>
2021-12-11 11:56:18 +0100

    add axio

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.scss';
-import TitreH1 from './components/Titres/TitreH1/TitreH1';
+import PaysManager from './containers/PaysManager/PaysManager';
 
 function App() {
   return (
-    <div className="container">
-      <TitreH1>Liste des pays du monde</TitreH1>
-    </div>
+    <PaysManager/>
   );
 }
 
