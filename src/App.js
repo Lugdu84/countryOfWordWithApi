db1ad83fb645ae0480bd9b2130c589518d28c67a
David Grammatico <david.lugdunum@gmail.com>
2021-12-11 11:30:08 +0100

    first commit, and add boostrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import TitreH1 from './components/Titres/TitreH1/TitreH1';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <TitreH1>Liste des pays du monde</TitreH1>
     </div>
   );
 }
